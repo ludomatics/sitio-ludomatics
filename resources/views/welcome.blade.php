fce70b9ee801588b9860c55a147914d8ecca2e3c
rhofp <rhodfra@gmail.com>
2019-10-06 21:43:44 -0500

    Comienzo lista de alumnos con escuela

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,18 +98,155 @@
     </div>
   </section>
 
-    <!-- Contacto -->
-    <section>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 col-md-12">
-            <div class="map-container text-center">
-              <h6>Morelos núm. 6, Tezompa, Chalco Edo. Mex. 56624</h6>
-              <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2250.8089439453056!2d-98.95851777570967!3d19.20799082969903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce1b3e08868d49%3A0x25aeef651fc343f0!2sLudomatics!5e0!3m2!1ses-419!2smx!4v1543111696799" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
+  <section class="bg-blue">
+    <div class="container">
+      <div class="row per-margin-20">
+        <div class="col-lg-12 col-md-12 col-sm-12 text-center mb-3">
+          <h3 class="section-heading text-uppercase">Felicitamos a nuestros alumnos por haber alcanzado su meta</h3>
+          <h4>Curso COMIPEMS</h4>
+        </div>
+      </div>
+      <div class="row">
+
+        <div class="col-lg-3 col-md-2 col-sm-12">
+            <h5>Generación 2012 y 2013</h5>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Jorge Garcia</h5>
+            <h6>Cecyt 2 IPN</h6>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Rodrigo Francisco</h5>
+            <h6>ENP 1 UNAM</h6>
+        </div>        
+
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Carlos Ramírez R.</h5>
+            <h6>ENP 6 UNAM</h6>
+        </div>    
+
+
+        <div class="col-lg-3 col-md-2 col-sm-12">
+            <h5>Generacion 2014 y 2015</h5>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Cassiel García</h5>
+            <h6>ENP 6 UNAM</h6>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Miguel Francisco</h5>
+            <h6>CCH Sur UNAM</h6>
+        </div>        
+
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Angela</h5>
+            <h6>CCH Sur UNAM</h6>
+        </div>   
+
+        <div class="col-lg-3 col-md-2 col-sm-12">
+            <!--h5>Generacion 2014 y 2015</h5-->
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Lorena</h5>
+            <h6>CCH Oriente UNAM</h6>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Ximena</h5>
+            <h6>CECYT 15 IPN</h6>
+        </div>        
+
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <!--h5>Angela</h5>
+            <h6>CCH Sur UNAM</h6-->
+        </div>   
+
+      </div>
+    </div> 
+  </section>
+
+
+  <section class="bg-orange">
+    <div class="container">
+      <div class="row per-margin-20">
+        <div class="col-lg-12 col-md-12 col-sm-12 text-center mb-3">
+          <h3 class="section-heading text-uppercase">La constancia es la clave del éxito</h3>
+          <h4>Curso de ingreso a la universidad</h4>
+        </div>
+      </div>
+      <div class="row">
+
+        <div class="col-lg-3 col-md-2 col-sm-12">
+            <h5>Generación 2012 y 2013</h5>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Jorge Garcia</h5>
+            <h6>Cecyt 2 IPN</h6>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Rodrigo Francisco</h5>
+            <h6>ENP 1 UNAM</h6>
+        </div>        
+
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Carlos Ramírez R.</h5>
+            <h6>ENP 6 UNAM</h6>
+        </div>    
+
+
+        <div class="col-lg-3 col-md-2 col-sm-12">
+            <h5>Generacion 2014 y 2015</h5>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Cassiel García</h5>
+            <h6>ENP 6 UNAM</h6>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Miguel Francisco</h5>
+            <h6>CCH Sur UNAM</h6>
+        </div>        
+
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Angela</h5>
+            <h6>CCH Sur UNAM</h6>
+        </div>   
+
+        <div class="col-lg-3 col-md-2 col-sm-12">
+            <!--h5>Generacion 2014 y 2015</h5-->
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Lorena</h5>
+            <h6>CCH Oriente UNAM</h6>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <h5>Ximena</h5>
+            <h6>CECYT 15 IPN</h6>
+        </div>        
+
+        <div class="col-lg-3 col-md-5 col-sm-12 text-center">
+            <!--h5>Angela</h5>
+            <h6>CCH Sur UNAM</h6-->
+        </div>   
+
+      </div>
+    </div> 
+  </section>
+
+  <!-- Contacto -->
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <div class="map-container text-center">
+            <h6>Morelos núm. 6, Tezompa, Chalco Edo. Mex. 56624</h6>
+            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2250.8089439453056!2d-98.95851777570967!3d19.20799082969903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce1b3e08868d49%3A0x25aeef651fc343f0!2sLudomatics!5e0!3m2!1ses-419!2smx!4v1543111696799" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
         </div>
       </div>
+    </div>
   </section>
   
+
+
+
+
 @endsection
