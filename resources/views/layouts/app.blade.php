a0e1d4a0fef98c0800883174b552d8bc01d050e3
rhofp <rhodfra@gmail.com>
2019-10-06 23:04:58 -0500

    Agrego asset de logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand" href="/">
-          <img src="img/logo/ludomatics.png" alt="ludomatics" class="logo">
+          <img src="{{asset('img/logo/ludomatics.png')}}" alt="ludomatics" class="logo">
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fas fa-bars"></i>
